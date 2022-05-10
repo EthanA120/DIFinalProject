ba8227ab9426ccb3496faaf59fe98f0c6a56da16
Ethan <EthanA120@Gmail.com>
2022-05-10 23:44:21 +0300

    In progress...

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class TaskForm(FlaskForm):
-    task = StringField('NewTask', validators=[DataRequired()])
-    image = StringField('Image', validators=[DataRequired()])
-    submit = SubmitField('AddButton')
+class RegisterForm(FlaskForm):
+    user_name = StringField('NewTask', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
 
 
-class ClearForm(FlaskForm):
-    clear = SubmitField('Clear')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.models import db
-# from app.forms import *
+from app.forms import RegisterForm, LoginForm
 from flask import render_template, redirect, url_for, session, request, flash
 
 db.create_all()
@@ -10,3 +10,15 @@ db.session.commit()
 @app.route('/', methods=["POST", "GET"])
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    form = RegisterForm()
+    return render_template('loginRegister/register.html', form=form)
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    return render_template('loginRegister/login.html', form=form)
