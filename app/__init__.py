da54717b756245927380e436b4fddc4f4daadfb6
Ethan <EthanA120@Gmail.com>
2022-05-13 20:03:40 +0300

    In progress...

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from flask_ngrok import run_with_ngrok
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+# run_with_ngrok(app, '296WTGEAyc728DZUXkdEpIQ4S2x_7Q7q7KVMNfKkaqAJyvb5f')
 
 app.config.from_object(Config)
 bootstrap = Bootstrap5(app)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from sqlalchemy import exc
 from app import app, login_mngr
 from app.models import db, Player, Score
 from app.forms import RegisterForm, LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask import render_template, redirect, url_for, session, request, flash
 
 
@@ -22,13 +22,12 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        try:
-            Player(username=form.user_name.data, email=form.email.data, password=form.password.data, score=Score()).add_user()
+        if not db.session.query(Player).filter_by(email=form.email.data).first():
+            Player(username=form.user_name.data, email=form.email.data, password=generate_password_hash(form.password.data), score=Score()).add_user()
+            return redirect(url_for('login'))
 
-            return redirect(url_for('index'))
-
-        except exc.IntegrityError:
-            flash('Username or email are already in use, please try again', 'warning')
+        else:
+            flash('Username or email are already in use, please try again.', 'warning')
 
     return render_template('loginRegister/register.html', form=form)
 
@@ -43,8 +42,8 @@ def login():
     if form.validate_on_submit():
         user = db.session.query(Player).filter_by(email=form.email.data).first()
 
-        if user is None or not user.password == form.password.data:
-            flash('Invalid username or password', 'warning')
+        if user is None or not check_password_hash(user.password, form.password.data):
+            flash('Invalid username or password.', 'warning')
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
@@ -54,6 +53,7 @@ def login():
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
