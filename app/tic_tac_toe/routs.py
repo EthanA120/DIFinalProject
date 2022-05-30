6dec797b4b31d252952355a7b70edfdca8abf204
Ethan <EthanA120@Gmail.com>
2022-05-30 19:55:55 +0300

    Final

--- app/tic_tac_toe/routs.py
+++ app/tic_tac_toe/routs.py
@@ -2,7 +2,7 @@ from app import db
 from app.tic_tac_toe.models import TicTacToe
 from easyAI import Human_Player, AI_Player, Negamax
 from flask_login import login_required, current_user
-from flask import Blueprint, render_template, request, make_response
+from flask import Blueprint, render_template, request, make_response, session
 
 tic_tac_toe = Blueprint('tic_tac_toe', __name__)
 
@@ -22,17 +22,19 @@ def play_game():
             ai_move = ttt.get_move()
             ttt.play_move(ai_move)
 
-    if 'reset_score' in request.form or 'reset' in request.form:
+    if 'reset' in request.form or 'reset_score' in request.form:
         ttt.board = [0 for i in range(9)]
+        session['lock'] = False
 
     if 'reset_score' in request.form:
         current_user.score.tic_tac_toe = 0
         current_user.score.ctic_tac_toe = 0
         db.session.commit()
 
-    if ttt.is_over():
+    if ttt.is_over() and not session['lock']:
         msg = ttt.winner()
         db.session.commit()
+        session['lock'] = True
     else:
         msg = "It's your turn to play"
 
